d51288ed13040a69bf9d01f4dca1984a40200473
somratpro <abdulmonnafsomrat@gmail.com>
2020-08-08 15:46:09 +0600

    initialize

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+(function ($) {
+  'use strict';
+
+  // Preloader js    
+  $(window).on('load', function () {
+    $('.preloader').fadeOut(100);
+  });
+
+  // navigation
+  $(window).scroll(function () {
+    if ($('.navigation').offset().top > 1) {
+      $('.navigation').addClass('nav-bg');
+    } else {
+      $('.navigation').removeClass('nav-bg');
+    }
+  });
+
+  //  Search Form Open
+  $('#searchOpen').on('click', function () {
+    $('.search-wrapper').addClass('open');
+    setTimeout(function () {
+      $('.search-box').focus();
+    }, 400);
+  });
+  $('#searchClose').on('click', function () {
+    $('.search-wrapper').removeClass('open');
+  });
+
+})(jQuery);
